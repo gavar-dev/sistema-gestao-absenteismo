74fe2b409f5d2ea2b9c3ba0e457e4045ced3c55c
gavar-dev <gabriel.varella1327@hotmail.com>
2026-06-03 20:23:58 -0300

    faet: Subindo os arquivos base das pages de RH e Gestor.

---
+++ src/app/pages/admin-rh/solicitacoes-component/solicitacoes-component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-solicitacoes-component',
+  imports: [],
+  templateUrl: './solicitacoes-component.html',
+  styleUrl: './solicitacoes-component.css',
+})
+export class SolicitacoesComponent {}
