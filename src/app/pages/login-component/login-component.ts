a0ed42ba1dcded96d3fabac7e830e7a52217cd7e
Gabriel <gabriel.varella1327@hotmail.com>
2026-06-15 14:16:08 -0300

    feat(login-component): subindo a page de login

--- src/app/pages/login-component/login-component.ts
+++ src/app/pages/login-component/login-component.ts
@@ -1,9 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-component',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './login-component.html',
   styleUrl: './login-component.css',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  senhaVisivel = false;
+
+  constructor(private router: Router) {}
+
+  alternarVisibilidade(): void {
+    this.senhaVisivel = !this.senhaVisivel;
+  }
+
+  entrar(event: Event): void {
+    event.preventDefault();
+
+    // Por enquanto, esse login apenas simula a entrada no sistema.
+    // Quando criar autenticação real, a validação entra aqui.
+    this.router.navigate(['/']);
+  }
+}
