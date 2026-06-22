b833e839f260f4c67818cc691455ae9ac84c102a
gavar-dev <gabriel.varella1327@hotmail.com>
2026-06-22 19:58:00 -0300

    fix(header-component): Modificando o header para ficar coerente com a page que o usuario abrir.

--- src/app/shared/header-component/header-component.ts
+++ src/app/shared/header-component/header-component.ts
@@ -27,38 +27,65 @@ export class HeaderComponent {
         this.atualizarHeader(event.urlAfterRedirects);
       });
   }
-
+ 
   private atualizarHeader(url: string): void {
-    const headers: Record<string, { titulo: string; descricao: string }> = {
+    const headers: Record<string, { titulo: string; descricao: string, subtitulo: string }> = {
       '/': {
         titulo: 'Início',
+        subtitulo: 'Área do funcionário',
         descricao: 'Veja um resumo das suas informações na intranet.',
       },
       '/meus-pontos': {
         titulo: 'Meu ponto',
+        subtitulo: 'Área do funcionário',
         descricao: 'Acompanhe sua jornada, registre os horários do dia e corrija pendências quando necessário.',
       },
       '/solicitacao': {
         titulo: 'Solicitações',
+        subtitulo: 'Área do funcionário',
         descricao: 'Abra solicitações para correção de ponto, férias, justificativas e dados cadastrais.',
       },
       '/historico': {
         titulo: 'Histórico',
+        subtitulo: 'Área do funcionário',
         descricao: 'Consulte seus registros anteriores de ponto e solicitações.',
       },
       '/meus-dados': {
         titulo: 'Meus dados',
+        subtitulo: 'Área do funcionário',
         descricao: 'Visualize suas informações pessoais e profissionais.',
       },
       '/avisos': {
         titulo: 'Avisos',
+        subtitulo: 'Área do funcionário',
         descricao: 'Acompanhe comunicados importantes da empresa.',
+      },
+      '/gestao/inicio': {
+        titulo: 'Painel de absenteismo',
+        subtitulo: 'Área de Gestão dos funcionários',
+        descricao: 'Dashboard com a análise do absenteismo da empresa.',
+      },
+      '/gestao/funcionarios': {
+        titulo: 'Gestão dos Funcionarios',
+        subtitulo: 'Área de Gestão dos funcionários',
+        descricao: 'Dados dos funcionarios.',
+      },
+      '/gestao/solicitacoes': {
+        titulo: 'Solicitações',
+        subtitulo: 'Área de Gestão dos funcionários',
+        descricao: 'Solicitações feitas pelos funcionarios.',
+      },
+      '/gestao/cadastro': {
+        titulo: 'Cadastro dos funcionarios',
+        subtitulo: 'Área de Gestão dos funcionários',
+        descricao: 'Cadastrar novos funcionarios.',
       },
     };
 
     const header = headers[url] ?? headers['/'];
 
     this.titulo = header.titulo;
+    this.subtitulo = header.subtitulo;
     this.descricao = header.descricao;
   }
 }
