dbfa7901f5600706f42979ae41b5806d73914e42
Gabriel <gabriel.varella1327@hotmail.com>
2026-06-22 14:53:27 -0300

    feat(sidbard-admin): subindo o sidbar do gestor e do rh

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,13 +7,33 @@ import { MeusDadosComponente } from './pages/funcionario/meus-dados-componente/m
 import { HistoricoComponente } from './pages/funcionario/historico-componente/historico-componente';
 import { LoginComponent } from './pages/login-component/login-component';
 
+import { InicioComponent as AdminInicioComponent } from './pages/admin-rh/inicio-component/inicio-component';
+import { GestaoComponent } from './pages/admin-rh/gestao-component/gestao-component';
+import { SolicitacoesComponent } from './pages/admin-rh/solicitacoes-component/solicitacoes-component';
+import { CadastroFuncionarioComponent } from './pages/admin-rh/cadastro-funcionario-component/cadastro-funcionario-component';
+
 export const routes: Routes = [
-     { path: 'login', component: LoginComponent },
-     { path: '', component: InicioComponente },
-     { path: 'meus-pontos', component: MeuPontoComponente },
-     { path: 'solicitacao', component: SolicitacaoComponente },
-     { path: 'historico', component: HistoricoComponente },
-     { path: 'meus-dados', component: MeusDadosComponente },
-     { path: 'avisos', component: AvisoComponente },
-     { path: '**', redirectTo: '' }
+  { path: 'login', component: LoginComponent },
+
+  // Área do funcionário
+  { path: '', component: InicioComponente },
+  { path: 'meus-pontos', component: MeuPontoComponente },
+  { path: 'solicitacao', component: SolicitacaoComponente },
+  { path: 'historico', component: HistoricoComponente },
+  { path: 'meus-dados', component: MeusDadosComponente },
+  { path: 'avisos', component: AvisoComponente },
+
+  // Área de Gestão/RH
+  {
+    path: 'gestao',
+    children: [
+      { path: '', redirectTo: 'inicio', pathMatch: 'full' },
+      { path: 'inicio', component: AdminInicioComponent },
+      { path: 'funcionarios', component: GestaoComponent },
+      { path: 'solicitacoes', component: SolicitacoesComponent },
+      { path: 'cadastro', component: CadastroFuncionarioComponent }
+    ]
+  },
+
+  { path: '**', redirectTo: '' }
 ];
