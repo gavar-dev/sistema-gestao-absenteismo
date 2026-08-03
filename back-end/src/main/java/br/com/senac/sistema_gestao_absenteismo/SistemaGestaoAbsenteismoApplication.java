75748930dad72d96918625fc15b29e1f3c3c02b5
Gabriel Alves <gabriel.varella1327@hotmail.com>
2026-08-03 11:22:37 -0300

    feat: aplicação principal

---
+++ back-end/src/main/java/br/com/senac/sistema_gestao_absenteismo/SistemaGestaoAbsenteismoApplication.java
@@ -0,0 +1,13 @@
+package br.com.senac.sistema_gestao_absenteismo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGestaoAbsenteismoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaGestaoAbsenteismoApplication.class, args);
+	}
+
+}
